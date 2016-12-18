0dfc695b513432d2c6146255d15cf1e049ae136d
Jordan C <Jordan C>
2016-12-17 23:52:32 -0500

    Initial commit

---
+++ src/com/company/Node.java
@@ -0,0 +1,53 @@
+package com.company;
+
+
+public class Node
+{
+    private Object Data;
+    private Node LeftChild;
+    private Node RightChild;
+
+    public Node()
+    {
+        Data = null;
+        LeftChild = null;
+        RightChild = null;
+    }
+
+    public Node(Object data, Node left, Node right)
+    {
+        Data = data;
+        LeftChild = left;
+        RightChild = right;
+    }
+
+    public Object getData()
+    {
+        return Data;
+    }
+
+    public void setData(Object data)
+    {
+        Data = data;
+    }
+
+    public Node getLeftChild()
+    {
+        return LeftChild;
+    }
+
+    public void setLeftChild(Node leftChild)
+    {
+        LeftChild = leftChild;
+    }
+
+    public Node getRightChild()
+    {
+        return RightChild;
+    }
+
+    public void setRightChild(Node rightChild)
+    {
+        RightChild = rightChild;
+    }
+}
---
+++ src/com/company/Tree.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by the1_ on 12/16/2016.
+ */
+public class Tree
+{
+
+}
