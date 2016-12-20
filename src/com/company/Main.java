e637d357336800de94cffad384eba6b490a9045d
J-Cordova <jordan.m.cordova@gmail.com>
2016-12-19 23:09:17 -0500

    Add Recursive implementations of tree traversals and a method to generate dummy data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Tree tree = new Tree();
+	    tree.root = tree.CreateTestData();
+
+        System.out.println("In Order -----------");
+        System.out.println();
+        tree.RecursiveInorder(tree.root);
+
+
+        System.out.println();
+        System.out.println("Post Order ---------");
+        System.out.println();
+        tree.RecursivePostorder(tree.root);
+
+
+        System.out.println();
+        System.out.println("Pre Order ----------");
+        System.out.println();
+        tree.RecursivePreorder(tree.root);
     }
 }
--- src/com/company/Tree.java
+++ src/com/company/Tree.java
@@ -1,9 +1,74 @@
 package com.company;
 
-/**
- * Created by the1_ on 12/16/2016.
- */
 public class Tree
 {
+    Node root = new Node("I Am Root", null, null);
 
+    public Node getRoot()
+    {
+        return root;
+    }
+
+    public void setRoot(Node root)
+    {
+        this.root = root;
+    }
+
+
+    public Node CreateTestData()
+    {
+        Node node = new Node("test Root",
+                            new Node("Left 1", new Node("left1  left", null,null), new Node("left1 right", null, null)),
+                            new Node("Right 1", new Node("right  left", null,null), new Node("right1 right", null, null)));
+
+        return node;
+    }
+
+
+    public void RecursiveInorder(Node node)
+    {
+        if (node == null)
+            return;
+        RecursiveInorder(node.getLeftChild());
+        System.out.println(node.getData().toString());
+        RecursiveInorder(node.getRightChild());
+    }
+
+    public void RecursivePreorder(Node node)
+    {
+        if (node == null)
+            return;
+        System.out.println(node.getData().toString());
+        RecursivePreorder(node.getLeftChild());
+        RecursivePreorder(node.getRightChild());
+    }
+
+    public void RecursivePostorder(Node node)
+    {
+        if (node == null)
+            return;
+        RecursivePostorder(node.getLeftChild());
+        RecursivePostorder(node.getRightChild());
+        System.out.println(node.getData().toString());
+    }
+
+    public void IterativeInorder(Node node)
+    {
+
+    }
+
+    public void IterativePreorder(Node node)
+    {
+
+    }
+
+    public void IterativePostorder(Node node)
+    {
+
+    }
+
+    public Object Search()
+    {
+        return null;
+    }
 }
